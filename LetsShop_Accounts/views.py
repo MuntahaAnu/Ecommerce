72267ea45215c033e3896a130447255832586de2
Lamia Muntaha <lamiamuntaha1550@gmail.com>
2024-08-09 12:29:15 +0600

    reg handle

--- LetsShop_Accounts/views.py
+++ LetsShop_Accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from django.contrib import messages
  
 def LOGIN(request):
     return render(request,'Accounts/login.html')
@@ -13,9 +13,15 @@ def REG(request):
         Email=request.POST.get('email')
         Pass=request.POST.get('pass')
         Pass1=request.POST.get('pass1')
-        if Pass == Pass1:
-           user = User.objects.create(first_name=First_name,last_name=Last_name,username=UserName,email=Email,password=Pass)
-           user.set_password(Pass)
-           user.save()
+        if UserName is not None:
+            if User.objects.filter(username=UserName).exists():
+                messages.warning(request, "Your Username already taken! Try New.")
+            elif User.objects.filter(email=Email).exists():
+                messages.warning(request, "Your Email already taken! Try New.")
+            else:
+                if Pass == Pass1:
+                   user = User.objects.create(first_name=First_name,last_name=Last_name,username=UserName,email=Email,password=Pass)
+                   user.set_password(Pass)
+                   user.save()
 
     return render(request,'Accounts/registration.html')
