7c1f821d6e74281cce8f8d6e2834c41648073905
Lamia Muntaha <lamiamuntaha1550@gmail.com>
2024-08-09 12:04:45 +0600

    Create User

--- LetsShop_Accounts/views.py
+++ LetsShop_Accounts/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+ 
 def LOGIN(request):
     return render(request,'Accounts/login.html')
 
 def REG(request):
+    if request.method =='POST':
+        First_name=request.POST.get('first')
+        Last_name=request.POST.get('last')
+        UserName=request.POST.get('name')
+        Email=request.POST.get('email')
+        Pass=request.POST.get('pass')
+        Pass1=request.POST.get('pass1')
+        if Pass == Pass1:
+           user = User.objects.create(first_name=First_name,last_name=Last_name,username=UserName,email=Email,password=Pass)
+           user.set_password(Pass)
+           user.save()
+
     return render(request,'Accounts/registration.html')
